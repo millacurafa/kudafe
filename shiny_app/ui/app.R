0c91ef67364a791616c529e8c462977d90a9a07c
Felipe A. Millacura <41435710+millacurafa@users.noreply.github.com>
2020-04-14 08:55:58 +0100

    adds functionality for four tabs with dummy data, delimiting via comments

--- shiny_app/ui/app.R
+++ shiny_app/ui/app.R
@@ -1,20 +1,30 @@
 library(shiny)
+library(shinythemes)
+# library(dplyr)
+# library(ggplot2)
 
 
-source("ui_server/app_server.R")
+source("../ui_server/app_server.R")
 # Define UI for app that draws a histogram ----
 ui <- fluidPage(
   
-  # App title ----
-  titlePanel("Hello Shiny!"),
+  #Theme to be chosen from shinithemes library
+  theme = shinytheme("darkly"),
   
-  # Sidebar layout with input and output definitions ----
+  # App title ----
+  titlePanel("CodeClan Webinar's web trafic analysis"),
+  #Here we start working with the first tabs by generating a TAB holder/panel
+  tabsetPanel(
+    # ---- First tab to be created ----
+    tabPanel("Random Sample Plot",
+  
+  # ---- Sidebar layout with input and output definitions ----
   sidebarLayout(
     
-    # Sidebar panel for inputs ----
+    # ---- Sidebar panel for inputs ----
     sidebarPanel(
       
-      # Input: Slider for the number of bins ----
+      # ---- Input: Slider for the number of bins ----
       sliderInput(inputId = "bins",
                   label = "Number of bins:",
                   min = 1,
@@ -22,15 +32,33 @@ ui <- fluidPage(
                   value = 30)
       
     ),
-    
-    # Main panel for displaying outputs ----
+    # ---- Ends Sidebar panel for inputs ----
+    # ---- Main panel for displaying outputs ----
     mainPanel(
       
       # Output: Histogram ----
       plotOutput(outputId = "distPlot")
       
-    )
+        )
+    # ---- Ends Main panel for displaying outputs ----
+      )
+    #Closes first tab
+    ),
+  # ---- Second tab to be created ----
+  tabPanel("Random Dot Plot",
+           #Closes second tab
+           ),
+  # ---- Third tab to be created ----
+  tabPanel("Random General Plot",
+           #Closes third tab
+  ),
+  # ---- Third tab to be created ----
+  tabPanel("Random Information",
+           #Closes fourth tab
   )
+    #Closes tabset panel
+  )
+#Closes fluidPage
 )
 
 shinyApp(ui, server)
