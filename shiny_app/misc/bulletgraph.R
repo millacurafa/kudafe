c9a048cadc41fd67b7645b7ba8389597709599b1
Felipe A. Millacura <41435710+millacurafa@users.noreply.github.com>
2020-04-15 08:58:59 +0100

    adds d3dashboard library and graphs

---
+++ shiny_app/misc/bulletgraph.R
@@ -0,0 +1,24 @@
+# library(devtools)
+# library(htmltools)
+# library(htmlwidgets)
+library(d3Dashboard)
+library(shiny)
+
+ytd2005 <- list(
+  title=list("Revenue", "Profit", "Order Size", "New Customers", "Satisfaction"),
+  subtitle=list("US$, in thousands", "%", "US$, average", "count", "out of 5"),
+  range=list(c(150, 225, 300),
+             c(20, 25, 30),
+             c(350, 500, 600),
+             c(1400, 2000, 2500),
+             c(3.5, 4.25, 5)),
+  measures=list(c(220, 270),
+                c(21, 23),
+                c(100, 320),
+                c(1000, 1650),
+                c(3.2, 4.7)),
+  markers=list(250, 26, 550, 2100, 4.2)
+)
+
+# Plot
+bulletGraph(ytd2005)
