467fdfdbb3a541984505c42762191a7d6b8d7758
Felipe A. Millacura <41435710+millacurafa@users.noreply.github.com>
2020-04-15 10:05:49 +0100

    adds last broken shiny

--- shiny_app/ui/app.R
+++ shiny_app/ui/app.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(shinythemes)
+# library(d3Dashboard)
 library(tidyverse)
 
 
@@ -57,6 +58,12 @@ ui <- fluidPage(
                                 choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3),
                                 selected = 1),
              hr(),
+             
+             #Starts fluidRow 
+             fluidRow(
+              #Starts a new column to hold symbols
+             column(6,
+             #Starts a new row
              fluidRow(
                icon("google", lib = "font-awesome"),
                ),
@@ -96,16 +103,39 @@ ui <- fluidPage(
                fluidRow(
                icon("reddit", lib = "font-awesome")
                ),
-               
-           #Closes sidebar panel
-           ),
+             ), #Closes column
+             #Starts second column to hold bulletgraph
+             column(6, "trial text"
+             #        ytd2005 <- list(
+             #          title=list("Revenue", "Profit", "Order Size", "New Customers", "Satisfaction"),
+             #          subtitle=list("US$, in thousands", "%", "US$, average", "count", "out of 5"),
+             #          range=list(c(150, 225, 300),
+             #                     c(20, 25, 30),
+             #                     c(350, 500, 600),
+             #                     c(1400, 2000, 2500),
+             #                     c(3.5, 4.25, 5)),
+             #          measures=list(c(220, 270),
+             #                        c(21, 23),
+             #                        c(100, 320),
+             #                        c(1000, 1650),
+             #                        c(3.2, 4.7)),
+             #          markers=list(250, 26, 550, 2100, 4.2)
+             #        ),
+             #        # Plot
+             #        bulletGraph(ytd2005)
+                    
+                    ) #Closes second column holding bulletgraph
+             
+             ) #Closes fluid row for icons and bullet graph
+             
+           ), #Closes sidebar panel
            #Starts main panel
            mainPanel(
            #
            #    Output: ----Histogram ----
            #   plotOutput(outputId = "dotPlot")
            #
-           # ),
+           # ), # ---- Closes Histogram ----
 
            #Closes main panel
             )
